9ba138ddad6c1f0145ed62903a4919b0777645f0
Suraj Jindal <surajjindal@Surajs-MacBook-Air.local>
2023-09-10 01:43:24 +0530

    some issue

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,15 +2,15 @@
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-// import { getCategories } from '@/services';
+import { getCategories } from '@/services';
 
 const Header = () => {
-    // const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-    // useEffect(() => {
-    //     getCategories()
-    //     .then((newcategories) => setCategories(newcategories))
-    // }, []);
+    useEffect(() => {
+        getCategories()
+        .then((newcategories) => setCategories(newcategories))
+    }, []);
 
   return (
     <div className='container mx-auto px-10 mb-8'>
@@ -22,7 +22,7 @@ const Header = () => {
                     </span>
                 </Link>
             </div>
-            {/* <div className='hidden md:float-left md:contents'>
+            <div className='hidden md:float-left md:contents'>
                 {categories.map((category) => (
                     <Link key={category?.slug} href={`/category/${category?.slug}`}>
                         <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
@@ -30,7 +30,7 @@ const Header = () => {
                         </span>
                     </Link>
                 ))}
-            </div> */}
+            </div>
         </div>
     </div>
   );
